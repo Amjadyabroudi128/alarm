1da97e8a934716ad2772b3502deae9c4985d1cc4
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-10-16 13:34:25 +0100

    home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+
+import 'homePage.dart';
 void main() {
   runApp(const MyApp());
 }
@@ -17,41 +19,3 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key,});
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  var currentTime;
-  final HourMinute = DateFormat("Hm");
-  @override
-  void initState() {
-    currentTime = DateTime.now();
-  }
-  @override
-  Widget build(BuildContext context) {
-    return  Scaffold(
-      body: Center(
-        child: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-               Icon(
-                 Icons.access_alarm_outlined,
-                 size: 90,
-               ),
-              Text("${HourMinute.format(currentTime)}",
-                style: TextStyle(
-                  fontSize: 45,
-                  fontWeight: FontWeight.bold
-                ),)
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
