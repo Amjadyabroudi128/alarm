9cc52bc8e1f355c33deaf7605985ec95433a1fe0
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-10-17 13:27:32 +0100

    icon size

--- lib/consts.dart
+++ lib/consts.dart
@@ -3,7 +3,7 @@
  import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-Icon clock = const Icon(Icons.access_alarm_outlined);
+Icon clock = const Icon(Icons.access_alarm_outlined, size: 90,);
 TextStyle timeStyle = const TextStyle(
     fontSize: 40, fontWeight: FontWeight.bold
 );
