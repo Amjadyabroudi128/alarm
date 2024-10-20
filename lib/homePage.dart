887df0bf8e30303b06e6a7daccb690d2f3cd8dae
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-10-20 14:55:19 +0100

    add scheduled notification to home page

--- lib/homePage.dart
+++ lib/homePage.dart
@@ -1,8 +1,7 @@
 import 'package:alarm/widgets/timeText.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:intl/intl.dart';
-
+import 'notification/NotificationHelper.dart';
 import 'consts.dart';
 
 class MyHomePage extends StatefulWidget {
@@ -13,24 +12,49 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final FlutterLocalNotificationsPlugin myNotifications = FlutterLocalNotificationsPlugin();
   var currentTime = DateTime.now();
-   TimeOfDay selectedTime = TimeOfDay.now();
+  TimeOfDay selectedTime = TimeOfDay.now();
   final hourandMinute = DateFormat("Hm");
+
+  @override
+  void initState() {
+    super.initState();
+    // Initialize notifications when the app starts
+    Notificationhelper.init();
+  }
+
   Future<void> _selectDate() async {
-    final TimeOfDay? timeOfDay = await showTimePicker(context: context,
-        initialTime: selectedTime,
-      initialEntryMode: TimePickerEntryMode.dial
+    final TimeOfDay? timeOfDay = await showTimePicker(
+      context: context,
+      initialTime: selectedTime,
+      initialEntryMode: TimePickerEntryMode.dial,
     );
-    if(timeOfDay != null) {
+    if (timeOfDay != null) {
       setState(() {
         selectedTime = timeOfDay;
       });
+
+      final now = DateTime.now();
+      final DateTime scheduledDate = DateTime(
+        now.year,
+        now.month,
+        now.day,
+        timeOfDay.hour,
+        timeOfDay.minute,
+      );
+      // Schedule the notification
+      await Notificationhelper.scheduledNoti(
+          "good day",  // Title of the notification
+          "it's time to Wake Up",  // Body of the notification
+          scheduledDate,  // The time the notification should trigger
+          true  // Android allow while idle
+      );
     }
-}
+  }
+
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       body: Center(
         child: SingleChildScrollView(
           child: Column(
@@ -41,12 +65,11 @@ class _MyHomePageState extends State<MyHomePage> {
               ElevatedButton(
                 style: ElevatedButton.styleFrom(
                   backgroundColor: btnClr,
-                  minimumSize: btnSize
+                  minimumSize: btnSize,
                 ),
                 onPressed: _selectDate,
-                child: Text("select Time", style: btnStyle,),
-
-              )
+                child: Text("Select Time", style: btnStyle,),
+              ),
             ],
           ),
         ),
@@ -54,4 +77,3 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
-
