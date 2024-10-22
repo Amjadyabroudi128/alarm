c6da1cc89d0e0aa9aae127b4814e8e0cc2c6fc4d
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-10-22 14:52:13 +0100

    theme class

--- lib/themes/ThemeClass.dart
+++ lib/themes/ThemeClass.dart
@@ -1,4 +1,15 @@
 
- class MyTheme {
+ import 'package:flutter/material.dart';
 
+import '../consts.dart';
+
+class MyTheme {
+ static final theme = ThemeData(
+  elevatedButtonTheme: ElevatedButtonThemeData(
+   style: ElevatedButton.styleFrom(
+    backgroundColor: btnClr,
+    minimumSize: btnSize,
+   )
+  )
+ );
  }
